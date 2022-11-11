c827eb58998ca3e0542a2ca2f16fe564659bb31f
Miti Nopnirapath <mitip.2022@mqf.smu.edu.sg>
2022-11-11 17:45:51 +0800

    added __version__

--- mqf_toolbox/__init__.py
+++ mqf_toolbox/__init__.py
@@ -1,1 +1,2 @@
 # lets goooo
+from ._version import __version__
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
 
+import re
+VERSIONFILE = "mqf_toolbox/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 setup(name='mqf_toolbox',
-      version='0.1.2',
+      version=verstr,
       description='Useful Stuff for SMU MQF',
       author='Miti Nopnirapath',
       author_email='mitip.2022@mqf.smu.edu.sg',
-      install_requires=["numpy",
-                        "pandas",
-                        "statsmodels",
-                        "matplotlib",
-                        "setuptools",
+      install_requires=["setuptools",
                         ],
-      packages=['mqf_toolbox', 'mqf_toolbox.asset_pricing'],
+      packages=['mqf_toolbox',
+                'mqf_toolbox.asset_pricing'],
       )
